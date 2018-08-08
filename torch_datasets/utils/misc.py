88cfce7868cc793ba34ca162c23b687440f1a58e
mingruimingrui <mingruimingrui@hotmail.com>
2018-08-08 15:25:43 +0800

    added recursive cuda for batches

--- torch_datasets/utils/misc.py
+++ torch_datasets/utils/misc.py
@@ -1,28 +1,18 @@
-
+import collections
 import torch
+from torch._six import string_classes
 
 
-def deepcuda(obj, device_idx=None):
-    """ Recursively copies a list, tuple, dict, module or tensor typed object into device memory
-    Think of deepcopy but for cuda tensor conversion
-    """
-    if obj is None:
+def cuda_batch(batch, device_idx=None):
+    if batch is None:
         return None
-
-    obj_is_tensor = torch.tensor._TensorBase in obj.__class__.__bases__
-    obj_is_module = torch.nn.modules.module.Module in obj.__class__.__bases__
-
-    if obj_is_tensor or obj_is_module:
-        return obj.cuda()
-
-    elif isinstance(obj, list):
-        return [deepcuda(o) for o in obj]
-
-    elif isinstance(obj, tuple):
-        return tuple(deepcuda(o) for o in obj)
-
-    elif isinstance(obj, dict):
-        return {k: deepcuda(v) for (k, v) in obj.items()}
-
+    elif isinstance(batch, torch.Tensor):
+        return batch.cuda(device_idx)
+    elif isinstance(batch, string_classes):
+        return batch
+    elif isinstance(batch, collections.Mapping):
+        return {k: cuda_batch(sample) for k, sample in batch.items()}
+    elif isinstance(batch, collections.Sequence):
+        return [cuda_batch(sample) for sample in batch]
     else:
-        raise TypeError('Got unimplemented type {}'.format(type(obj)))
+        return batch
